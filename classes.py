e45a51aa88af2a6d182c69a795639f1d28879b1e
jayma1n <134546865+jayma1n@users.noreply.github.com>
2025-03-05 09:57:44 -0600

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+class Survivor(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,60])
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.color_key = WHITE
+        self.color = BLACK
+        
+        pygame.draw.rect(self.image, self.color_key, [0,0,10,10])
+
+        def draw(self, screen):
+            screen.blit(self.image, self.rect)
