ad4e2cb8d27675daf9a1dd1ed8db32ff94b68e9e
jayma1n <134546865+jayma1n@users.noreply.github.com>
2025-03-07 17:51:27 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,7 +20,7 @@ class Survivor(pygame.sprite.Sprite):
 class Companion(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([40,60])
+        self.image = pygame.Surface([40,40])
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
         self.color_key = WHITE
